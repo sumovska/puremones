daf608ccad0f73081d657a3ed7d2381531e796aa
Nataliia <sumovskaya@gmail.com>
2016-04-30 10:35:54 +0700

    new files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
+$(function () {
 
-$(function(){
-    
-    if($('#content').height()>$('#sidebar').height()) { $('#sidebar').height($('#content').height()); }
-    
+	if ($('#content').height() > $('#sidebar').height()) {
+		$('#sidebar').height($('#content').height());
+	}
+
+	/** Toggle Main Menu */
+	$('#main-menu ').each(function () {
+		$(this).append('<span class="toggle"></span>');
+	});
+
 });
 
