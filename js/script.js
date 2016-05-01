a2fecbb46c2ba402cdbd1e90a673cdc7fae61a37
Nataliia <sumovskaya@gmail.com>
2016-05-01 11:43:08 +0700

    responsive version

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
-$(function () {
+/**
+ * On document ready
+ */
+
+$(document).ready(function () {
 
 	if ($('#content').height() > $('#sidebar').height()) {
 		$('#sidebar').height($('#content').height());
 	}
 
 	/** Toggle Main Menu */
-	$('#main-menu ').each(function () {
+	$('#header').each(function () {
+		var _body = $('body');
 		$(this).append('<span class="toggle"></span>');
+		$('.toggle', this).each(function () {
+			function handler(e) {
+				if (!($(e.target).hasClass('nav') || ($(e.target).closest('.nav').length > 0))) {
+					_body.removeClass('nav-open');
+					_body.off('click touchstart', handler);
+				}
+			}
+
+			$(this).on('click', function () {
+				if (_body.hasClass('nav-open')) {
+					_body.off('click touchstart', handler).removeClass('nav-open');
+				} else {
+					_body.on('click touchstart', handler).addClass('nav-open');
+				}
+				return false;
+			});
+		});
 	});
 
 });
